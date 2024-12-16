0aa4044d3e5f00972a4126e4e53e9a54b0b51b03
hunter123348 <muddasirstore@gmail.com>
2024-12-16 13:01:57 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -10,14 +10,14 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "923444844060"
-global.ownername = "Qasim Ali"
+global.ownernomer = "923131277656"
+global.ownername = "HUNTER HACKER"
 global.ytname = "YT: GlobalTechInfo"
 global.socialm = "GitHub: GlobalTechInfo"
 global.location = "Pakistan, Karachi, Karachi"
 
-global.ownernumber = '923444844060'  //creator number
-global.ownername = 'Qasim Ali' //owner name
+global.ownernumber = '923131277656'  //creator number
+global.ownername = 'HUNTER HACKER' //owner name
 global.botname = 'XeonBug-V8' //name of the bot
 
 //sticker details
